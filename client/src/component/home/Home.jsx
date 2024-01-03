e7f1fc0b74c551f935de1d899c05b5f40301dd12
pranavhajare <hajare.pranav@gmail.com>
2024-01-03 14:09:54 +0530

    create Carousel

--- client/src/component/home/Home.jsx
+++ client/src/component/home/Home.jsx
@@ -1,9 +1,25 @@
-
 // components
 import Navbar from "./Navbar";
+import Banner from "./Banner";
+
+
+import {Box,styled} from '@mui/material';
+
+
+const Component  = styled(Box)`
+    padding:10px;
+    background: #F2F2F2;
+`;
 
 const Home = () => {
-    return <Navbar/>
+    return(
+        <> 
+                <Navbar/>
+                <Component>
+                    <Banner/>
+                </Component>
+        </>
+    )
 }
 
 export default Home;
